3288ca7f57342df90f2e73833baf84ea6a1dd9b5
NadiaMimoun <mimounnadia205@gmail.com>
2021-10-04 21:32:51 -0500

    update folder

--- w1d5/test.js
+++ w1d5/test.js
@@ -1,2 +1,12 @@
-const assert = require('chai').assert;
-const Chef = require('./lab5.js');
+describe("sum",function(){
+    it("takes an array and return the sum of the elements",
+    function(){
+        assert.equal("10",sum(["1","2","3","4"]))
+    })
+});
+describe("multiply",function(){
+    it("takes an array and return the multiple of the elements",
+    function(){
+        assert.equal("256",sum(["1","2","3","4"]))
+    })
+});
