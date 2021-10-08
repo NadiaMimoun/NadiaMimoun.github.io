cfe3bb9014ef740e843d64e9259c7beeae95a46b
NadiaMimoun <mimounnadia205@gmail.com>
2021-10-07 21:25:25 -0500

    add lab wed4

---
+++ test.js
@@ -0,0 +1,39 @@
+/*x=1;
+var a=1;
+var b=10;
+var c=function(a,b,c){
+    console.log(x);
+    console.log(a);
+    var f=function(a,b,c){
+        b=a;
+        console.log(b);
+        b=c;
+        var x=5;
+    }
+    f(a,b,c);
+    console.log(b);
+    var x=10;
+}
+c(8,9,10);
+console.log(b);
+console.log(x);
+
+
+var x=9;
+function myfunction(){
+    return x*x;
+}
+console.log(myfunction());
+x=5;
+console.log(myfunction());
+
+var foo=1;
+function bar(){
+    if(!foo){
+        var foo=10;
+    }
+    alert(foo);
+}
+bar();*/
+
+
