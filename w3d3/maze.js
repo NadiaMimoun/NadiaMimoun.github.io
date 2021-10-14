9353712bc1b9f5627989ac3fb7e7cc6cef1823a3
NadiaMimoun <mimounnadia205@gmail.com>
2021-10-13 22:07:18 -0500

    add w3d3

---
+++ w3d3/maze.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+var start=false;
+
+    $("#start").click(function(){
+        start=true;
+        $("#status").text('You still wining !');
+        $(".boundary").mousemove(function(){ lose(); })
+        if($('.boundary').hasClass('youlose')){
+            $('.boundary').removeClass('youlose');
+        }
+    });
+    $("#end").mousemove(function(){
+        if(start){
+            win();
+        }
+        else if((start==false)&&($('.boundary').hasClass('youlose'))){
+            alert("sorry you lose!");
+        }
+
+    });
+    function lose(){
+        if(start){
+            start = false;
+            $(".boundary").addClass('youlose');
+            $("#status").text('You Lose !');
+        }
+    }
+    function win(){
+        alert("you win!");
+        $("#status").text('You win !');
+        start = false;
+    }
+
+
+    
+})
